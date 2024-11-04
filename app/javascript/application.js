02067a40a16d0641d56e2005ef9a60a01a533e16
phbrugnolo <phbrugnolo@gmail.com>
2024-11-04 17:55:57 -0300

    Adição do jQuery via web, para possivel adaptação com axios

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
+import './sales'
+// import * as bootstrap from "bootstrap"
