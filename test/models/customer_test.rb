9047b4ace81e490dc27f297f846c7aaf069657a3
phbrugnolo <phbrugnolo@gmail.com>
2024-11-05 14:54:51 -0300

    Ajuste na matricula

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -2,13 +2,13 @@ require "test_helper"
 
 class CustomerTest < ActiveSupport::TestCase
   test "should create a valid customer" do
-    customer = Customer.new(name: "John Doe", matriculation: "123456")
+    customer = Customer.new(name: "John Doe", matriculation: "782139")
 
     assert customer.valid?
   end
 
   test "should not create customer without a name" do
-    customer = Customer.new(matriculation: "123456")
+    customer = Customer.new(matriculation: "782139")
 
     assert_not customer.valid?
     assert_includes customer.errors[:name], "can't be blank"
@@ -22,8 +22,8 @@ class CustomerTest < ActiveSupport::TestCase
   end
 
   test "should not create customer with duplicate matriculation" do
-    Customer.create!(name: "Jane Doe", matriculation: "123456")
-    duplicate_customer = Customer.new(name: "John Smith", matriculation: "123456")
+    Customer.create!(name: "Jane Doe", matriculation: "782139")
+    duplicate_customer = Customer.new(name: "John Smith", matriculation: "782139")
 
     assert_not duplicate_customer.valid?
     assert_includes duplicate_customer.errors[:matriculation], "has already been taken"
